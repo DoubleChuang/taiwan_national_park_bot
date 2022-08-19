dd4ddc90eb76d74ac8b1cd484cdfcfd4f6558357
Double.c~ <double.c@inwinstack.com>
2022-08-20 00:03:46 +0800

    fix: wait 7AM

--- tnpb/__main2__.py
+++ tnpb/__main2__.py
@@ -146,6 +146,16 @@ class TNPv2Bot:
         num_member = self._chrome.find_element_by_xpath('//*[@id="ContentPlaceHolder1_teams_count"]').get_attribute("value")
         if int(num_member) < 2:
             self._chrome.find_element_by_id("lineonechk").click()
+
+        now_utc8 = datetime.utcnow() + relativedelta(hours=8)
+        this_7am = datetime(year=now_utc8.year, month=now_utc8.month, day=now_utc8.day, hour=7)        
+        delta_sec = (this_7am - now_utc8).seconds
+        logger.info(f"{this_7am} - {now_utc8} = {delta_sec}")
+
+        if now_utc8.hour < 7:      
+            logger.info(f"sleep: {delta_sec}")
+            time.sleep(delta_sec)
+        
         self._chrome.find_element_by_id(
             "ContentPlaceHolder1_btnsetp2upnext").click()
 
